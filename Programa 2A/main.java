b0693ed99c3bf949e58136be8bfaa248ee6e15c5
Davi de Paula Oliveira <davi.miau@gmail.com>
2021-09-18 13:26:43 -0300

    Update main.java
    Corrigi a indentação.

--- Programa 2A/main.java
+++ Programa 2A/main.java
@@ -27,11 +27,11 @@ public class main {
 		
 		for(int i = 0; i < N; i++) {
 			teams[i] = new team();
-            teams[i].numero = i + 1;
-            teams[i].pontuacaoTotal = 0;
-            teams[i].totalCestasFeitas = 0;
-            teams[i].totalCestasRecebidas = 0;
-        }
+            		teams[i].numero = i + 1;
+            		teams[i].pontuacaoTotal = 0;
+            		teams[i].totalCestasFeitas = 0;
+            		teams[i].totalCestasRecebidas = 0;
+        	}
 		
 		for(int i = 1; i < linhas.length; i++) {
 			conteudo = linhas[i].split(" ");
@@ -42,17 +42,17 @@ public class main {
 			W = Integer.parseInt(conteudo[3]);
 			
 			teams[X - 1].totalCestasFeitas += Y;
-            teams[X - 1].totalCestasRecebidas += W;
-            teams[Z - 1].totalCestasFeitas += W;
-            teams[Z - 1].totalCestasRecebidas += Y;
+            		teams[X - 1].totalCestasRecebidas += W;
+            		teams[Z - 1].totalCestasFeitas += W;
+            		teams[Z - 1].totalCestasRecebidas += Y;
             
-            if(Y > W) {
-                teams[X - 1].pontuacaoTotal += 2;
-                teams[Z - 1].pontuacaoTotal += 1;
-            } else {
-                teams[X - 1].pontuacaoTotal += 1;
-                teams[Z - 1].pontuacaoTotal += 2;
-            }
+		        if(Y > W) {
+				teams[X - 1].pontuacaoTotal += 2;
+				teams[Z - 1].pontuacaoTotal += 1;
+		        } else {
+				teams[X - 1].pontuacaoTotal += 1;
+				teams[Z - 1].pontuacaoTotal += 2;
+		        }
 		}
 		
 		Arrays.sort(teams, new Comparator<team>() {
@@ -61,41 +61,41 @@ public class main {
 				double cestaAverage1;
 				double cestaAverage2;
 				
-			    if(primeiroteam.pontuacaoTotal > segundoteam.pontuacaoTotal) {
-			    	return -1;
-			    } else if(primeiroteam.pontuacaoTotal == segundoteam.pontuacaoTotal) {
-			        if(primeiroteam.totalCestasRecebidas != 0) {
-			        	cestaAverage1 = (double)primeiroteam.totalCestasFeitas / (double)primeiroteam.totalCestasRecebidas;
-			        } else {
-			        	cestaAverage1 = primeiroteam.totalCestasFeitas;
-			        }
+			        if(primeiroteam.pontuacaoTotal > segundoteam.pontuacaoTotal) {
+			    		return -1;
+			        } else if(primeiroteam.pontuacaoTotal == segundoteam.pontuacaoTotal) {
+					if(primeiroteam.totalCestasRecebidas != 0) {
+						cestaAverage1 = (double)primeiroteam.totalCestasFeitas / (double)primeiroteam.totalCestasRecebidas;
+					} else {
+						cestaAverage1 = primeiroteam.totalCestasFeitas;
+					}
 
-			        if(segundoteam.totalCestasRecebidas != 0) {
-			        	cestaAverage2 = (double)segundoteam.totalCestasFeitas / (double)segundoteam.totalCestasRecebidas;
-			        } else {
-			        	cestaAverage2 = segundoteam.totalCestasFeitas;
-			        }
+					if(segundoteam.totalCestasRecebidas != 0) {
+						cestaAverage2 = (double)segundoteam.totalCestasFeitas / (double)segundoteam.totalCestasRecebidas;
+					} else {
+						cestaAverage2 = segundoteam.totalCestasFeitas;
+					}
 
-			        if(cestaAverage1 > cestaAverage2) {
-			        	return -1;
-			        } else if(cestaAverage1 == cestaAverage2) {
-			            if(primeiroteam.totalCestasFeitas > segundoteam.totalCestasFeitas) {
-			            	return -1;
-			            } else if(primeiroteam.totalCestasFeitas == segundoteam.totalCestasFeitas) {
-			                if(primeiroteam.numero < segundoteam.numero) {
-			                	return -1;
-			                }
-			                
-			                return 1;
-			            }
-			            	
-			            return 1;
-			        }
+					if(cestaAverage1 > cestaAverage2) {
+						return -1;
+					} else if(cestaAverage1 == cestaAverage2) {
+					    if(primeiroteam.totalCestasFeitas > segundoteam.totalCestasFeitas) {
+						return -1;
+					    } else if(primeiroteam.totalCestasFeitas == segundoteam.totalCestasFeitas) {
+						if(primeiroteam.numero < segundoteam.numero) {
+							return -1;
+						}
 
-			        return 1;
-			    }
+						return 1;
+					    }
 
-			    return 1;
+					    return 1;
+					}
+
+					return 1;
+			    	}
+
+			    	return 1;
 			}
 		});
 		
